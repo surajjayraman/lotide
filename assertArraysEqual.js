15cdf81acc391126d161fb0be4c0aec96f411544
surajjayraman <suraj.jayraman@gmail.com>
2021-08-18 20:04:05 -0400

    implemented assertion fn using eqArrays fn

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const eqArrays = (array1, array2) => {
+  return (array1.length === array2.length) && array1.every(function(element, index) {
+    return element === array2[index];
+  });
+};
+
+// FUNCTION IMPLEMENTATION
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+  
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+  
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+  
+  
