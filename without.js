0acd2e674b7a732ad6e4dc2225553dba7303e8e5
surajjayraman <suraj.jayraman@gmail.com>
2021-08-18 21:04:54 -0400

    implemented without fn

---
+++ without.js
@@ -0,0 +1,31 @@
+const eqArrays = (array1, array2) => {
+  return (array1.length === array2.length) && array1.every(function(element, index) {
+    return element === array2[index];
+  });
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const without = (source, itemsToRemove) => {
+  let removedArray = [];
+  for (let elm of source) {
+    if (!itemsToRemove.includes(elm)) {
+      removedArray.push(elm);
+    }
+  }
+  return removedArray;
+};
+
+without([1, 2, 3], [1]); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
