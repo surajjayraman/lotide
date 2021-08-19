c4060e6b637735486044b93992f8426b3d66864f
surajjayraman <suraj.jayraman@gmail.com>
2021-08-19 17:39:44 -0400

    implemented findKeyByValue fn

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+  
+//Consider using the Object.keys function to help you easily search through all the object keys.
+//Consider using for...of to loop over the keys returned by Object.keys.
+
+const findKeyByValue = (object, value) => {
+  for (const key of Object.keys(object)) {
+    console.log(key);
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
