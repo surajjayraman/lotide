0a0fecd3bf657ffd72569c0b60e49b95e538925e
surajjayraman <suraj.jayraman@gmail.com>
2021-08-23 18:27:44 -0400

    refactored assertArraysEqual and added modularized test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,8 +1,5 @@
-const eqArrays = (array1, array2) => {
-  return (array1.length === array2.length) && array1.every(function(element, index) {
-    return element === array2[index];
-  });
-};
+const eqArrays = require('./eqArrays');
+
 
 // FUNCTION IMPLEMENTATION
 const assertArraysEqual = function(actual, expected) {
@@ -13,10 +10,6 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
   
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-  
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+module.exports = assertArraysEqual;
   
   
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+  
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+assertArraysEqual(["This", "is", "not", "equal"], ["This", "is", "not", "equall"]); // false
