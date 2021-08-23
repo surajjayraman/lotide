ba714f167068ed323a3545fcac1a2bf43dee1e86
surajjayraman <suraj.jayraman@gmail.com>
2021-08-23 17:53:43 -0400

    refactored eqArrays and added modularized test

--- test/tailTest.js
+++ test/tailTest.js
@@ -3,7 +3,7 @@ const tail = require('../tail');
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 // no need to capture the return value since we are not checking it
-assertEqual(tail(words).length,2); 
+assertEqual(tail(words).length,2);
 // original array should still have 3 elements!
 assertEqual(words.length, 3);
 //Single element and Empty Array checks
