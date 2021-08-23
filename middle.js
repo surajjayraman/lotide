2c789f60637b931bfb0769689031b2560db410fe
surajjayraman <suraj.jayraman@gmail.com>
2021-08-23 18:52:52 -0400

    refactored middle.js and added modularized test

--- middle.js
+++ middle.js
@@ -1,18 +1,3 @@
-const eqArrays = (array1, array2) => {
-  return (array1.length === array2.length) && array1.every(function(element, index) {
-    return element === array2[index];
-  });
-};
-  
-// FUNCTION IMPLEMENTATION
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑  Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // ACTUAL FUNCTION
 const middle = function(array) {
   //...
@@ -28,11 +13,4 @@ const middle = function(array) {
   return midElm;
 };
 
-middle([1]); // => []
-middle([1, 2]); // => []
-middle([1, 2, 3]); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
