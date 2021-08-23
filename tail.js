6fc68a50672b5c24e10a90c2a6961362c0429950
surajjayraman <suraj.jayraman@gmail.com>
2021-08-23 16:42:54 -0400

    refactored tail.js and added tailTest.js

--- tail.js
+++ tail.js
@@ -1,22 +1,7 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑  Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 //returns everything except for the first item (head) of the provided array.
 const tail = (input) => {
   const tailArr = input.slice(1);
   return tailArr;
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-//Single element and Empty Array checks
-console.log(tail([1]));
-console.log(tail([]));
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,11 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+// no need to capture the return value since we are not checking it
+assertEqual(tail(words).length,2); 
+// original array should still have 3 elements!
+assertEqual(words.length, 3);
+//Single element and Empty Array checks
+assertEqual(tail([1]).length, 0);
+assertEqual(tail([]).length, 0);
