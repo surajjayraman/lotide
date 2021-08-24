d9eece2eb1fbd482c437e49473c1aadedf31d398
surajjayraman <suraj.jayraman@gmail.com>
2021-08-23 21:18:39 -0400

    refactored tailTest using Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,20 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-// no need to capture the return value since we are not checking it
-assertEqual(tail(words).length,2);
-// original array should still have 3 elements!
-assertEqual(words.length, 3);
-//Single element and Empty Array checks
-assertEqual(tail([1]).length, 0);
-assertEqual(tail([]).length, 0);
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns [2] for [1, 2]", () => {
+    assert.deepEqual(tail([1, 2]), [2]);
+  });
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo' , 'Lighthouse', 'Labs'] ", () => {
+    assert.deepEqual(tail(['Yo Yo' , 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+
+});
+
+
+
